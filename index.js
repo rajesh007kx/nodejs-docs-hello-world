312d30b5d0a4ff6832cbb2c8fb72901769e4083a
rajesh007kx <rajesh.diehard@gmail.com>
2017-08-04 10:58:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World of EPS PaaS!");
+    response.end("Hello World of EPS PaaS, this shows my build Pipeline is working fine!");
 
 });
 
